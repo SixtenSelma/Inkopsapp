7af207009fd82f2aba618cc01d0faaab78502487
SixtenSelma <bahlsten@gmail.com>
2025-07-23 13:15:13 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,11 @@
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open('v1').then(cache => cache.addAll([
-      '/',
+      '/', 
       '/index.html',
       '/style.css',
-      '/main.js'
+      '/assets/main.js',
+      '/manifest.json'
     ]))
-  );
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
   );
 });
