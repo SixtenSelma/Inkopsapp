3f9952b15232dcf10e2ca74924d33a6ca2105c8d
SixtenSelma <bahlsten@gmail.com>
2025-07-27 01:37:38 +0200

    Update lists.js

--- assets/lists.js
+++ assets/lists.js
@@ -434,7 +434,8 @@ window.renderListDetail = function(i) {
         <span class="back-arrow"
               onclick="window.location.hash=''; renderAllLists()"
               title="Tillbaka">
-         <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" …>
+          <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24"
+               fill="none" stroke="#232323" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round">
             <polyline points="15 18 9 12 15 6"/>
           </svg>
         </span>
